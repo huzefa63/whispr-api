47fec0f36af98735e3065beb50a555e7a28fffb1
unknown <huzefaratlam63@gmail.com>
2025-06-09 23:21:18 +0530

    nothing

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const io = new Server(server,{
 app.use(cookieParser());
 
 app.use(express.json());
-app.use(cors({origin:'http://localhost:3000',credentials:true}));
+app.use(cors({origin:'*'}));
 app.use(express.urlencoded({extended:true}));
 
 app.use('/user',userRoute);
